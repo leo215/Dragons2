d8001003aa4da0c2a7f1387d26ac1c7f3e3d6551
dugrossy baptiste <dugrossy.baptiste@gmail.com>
2019-03-19 14:43:19 +0100

    work of Mohand and Willy added

---
+++ src/Controller/RechercheController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class RechercheController extends AbstractController
+{
+    /**
+     * @Route("/recherche", name="recherche")
+     */
+    public function index()
+    {
+        return $this->render('recherche/index.html.twig', [
+            'controller_name' => 'RechercheController',
+        ]);
+    }
+}
