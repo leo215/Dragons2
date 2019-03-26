1e81396adc6477bf00ab7b2464fd5eca0d9d942c
dugrossy baptiste <dugrossy.baptiste@gmail.com>
2019-03-26 14:06:10 +0100

    'Recherche' can now show the content of the DB.

--- src/Controller/RechercheController.php
+++ src/Controller/RechercheController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Dragon;
+use App\Entity\Personnage;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,8 +14,16 @@ class RechercheController extends AbstractController
      */
     public function index()
     {
+        $repository = $this->getDoctrine()->getRepository(Dragon::class);
+        $dragons = $repository->findAll();
+
+        $repository_2 = $this->getDoctrine()->getRepository(Personnage::class);
+        $personnages = $repository_2->findAll();
+
         return $this->render('recherche/index.html.twig', [
             'controller_name' => 'RechercheController',
+            'dragons' => $dragons,
+            'personnages' => $personnages,
         ]);
     }
 }
