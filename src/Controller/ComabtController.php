9559e1467ebc70bc6eb43490d6e166c008ca93cd
bezza <bezza0624@gmail.com>
2019-04-07 16:16:04 +0200

    ajoute la foncionnalite perdu de combat

--- src/Controller/ComabtController.php
+++ src/Controller/ComabtController.php
@@ -14,6 +14,19 @@ class ComabtController extends AbstractController
     {
         return $this->render('comabt/index.html.twig', [
             'controller_name' => 'ComabtController',
+            'personnagenom'=>"bezz",
+            'personnagevie'=>23,
+            'personnagepuissance'=>46,
+            'personnagedefense'=>767,
+            'personnagevitesse'=>556,
+            'personnagedex'=>6567,
+
+             'dragonnom'=>"bezz",
+            'dragonvie'=>23,
+            'dragonpuissance'=>46,
+            'dragondefense'=>767,
+            'dragonvitesse'=>556,
+            'dragondex'=>6567
         ]);
     }
 }
