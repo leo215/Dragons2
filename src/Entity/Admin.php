144e8b346addfcd6dc86af5575870d4a2b739f0e
leo215 <Sidneygovou@14>
2019-04-07 10:28:20 +0200

    Login Admin

--- src/Entity/Admin.php
+++ src/Entity/Admin.php
@@ -5,7 +5,7 @@ namespace App\Entity;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\UserInterface;
 /**
- * @ORM\Entity(repositoryClass="App\Repository\AdminRepository")
+ * @ORM\Entity()
  */
 class Admin implements UserInterface , \Serializable
 {
